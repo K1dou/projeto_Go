2714882db8d5d643764e08ebdfd1fa41825065d0
K1dou <hique1276@gmail.com>
2025-03-27 12:17:10 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/K1dou/projeto_Go.git
+
+go 1.24.1
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+  
+	test := os.Getenv("TEST")
+	fmt.Println("TEST:", test)
+	
+
+
+}
+ 
